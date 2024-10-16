cd60ea0ffa034e4c76b5bbbc9af80db15f87ed3c
Akash M Nandan <akash003nandan@gmail.com>
2024-10-17 02:06:38 +0530

    network section set (desktop view)

--- src/components/Network.tsx
+++ src/components/Network.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import icon from '@/public/arrow_up_right.png'
-
 import underline from '@/public/hero_underline.png'
+import network_image from '@/public/network_velammal_image.svg'
 
 const Network = () => {
   return (
@@ -21,9 +21,10 @@ const Network = () => {
         </div>
 
         {/* links and body */}
-        <div>
+        <div className='flex flex-row justify-between gap-[100px]'>
+
             {/* redirecting links */}
-            <div className='flex flex-col gap-[25px]'>
+            <div className='flex flex-col gap-[29px]'>
                 <button className="w-auto h-[60px] py-[12px] px-[20px] border-2 border-primary_blue flex flex-row items-center justify-center rounded-full gap-[15px]">
                     <p className="text-[20px] text-dark_text_blue">Velammal Medical College hospital and Research Institute</p>
                     <Image src={icon} alt="arrow up right"></Image>
@@ -51,8 +52,14 @@ const Network = () => {
             </div>
 
             {/* image and body */}
+            <div className='flex flex-col w-[510px] -mt-4'>
+                <Image src={network_image} alt='network_image' className='w-[510px] h-[350px]'></Image>
+                <p className='text-dark_text_blue font-[600] text-[24px] leading-[32px]'>Velammal Medical College hospital and Research Institute, Madurai</p>
+                <p className='text-body_text_white font-[400] text-[19px] leading-[26px]'>Delivering world-class medical care through compassion, innovation, and an unwavering commitment to improving lives. Explore all our healthcare services and experience the Velammal difference</p>
+            </div>
 
-        </div>
+
+        </div>          
     </div>
   )
 }
