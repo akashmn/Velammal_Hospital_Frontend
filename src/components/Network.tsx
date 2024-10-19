7c81657e61b9fac8a9a2be93e8fbc3095d9e1a32
Akash M Nandan <akash003nandan@gmail.com>
2024-10-19 13:41:54 +0530

    network heading text (mobile view)

--- src/components/Network.tsx
+++ src/components/Network.tsx
@@ -6,17 +6,17 @@ import network_image from '@/public/network_velammal_image.svg'
 const Network = () => {
   return (
     <div className='bg-secondary_white h-auto w-full px-[80px] py-[82px] flex flex-col items-center justify-center gap-[87px]'>
-        <div className='flex flex-col items-center justify-center gap-[80px]'>
+        <div className='flex flex-col items-center justify-center md:gap-[80px] gap-[25px]'>
             {/* heading & underline */}
-            <div className='flex flex-col items-center justify-center w-[1290px] gap-5'>
-                <p className='text-[#011632] text-center font-[600] text-[56px] leading-[21px] font-general-sans" style="leading-trim: both; text-edge: cap;"'>Velammal Healthcare Network</p>
-                <Image src={underline} alt='underline'></Image>
+            <div className='flex flex-col items-center justify-center md:w-[1290px] w-[410px] px-7 md:gap-5 gap-1'>
+                <p className='text-[#011632] text-center md:font-[600] font-bold md:text-[56px] text-[32px] md:leading-[21px]" style="leading-trim: both; text-edge: cap;"'>Velammal Healthcare Network</p>
+                <Image src={underline} alt='underline' className='h-[5px] w-[200px]'></Image>
             </div>
 
             {/* subheading & body */}
             <div className='flex flex-col items-center justify-center w-[1290px] gap-5'>
-                <p className='text-dark_text_blue text-center font-[600] text-[32px] leading-[38.4px]'>Compassionate care for a healthier tomorrow</p>
-                <p className='text-body_text_white text-center font-[500] text-[20px] leading-[26px]'>Delivering world-class medical care through compassion, innovation, and an unwavering commitment to improving lives. Explore all our healthcare services and experience the Velammal difference</p>
+                <p className='text-dark_text_blue text-center font-[600] md:text-[32px] text-[15px] leading-[38.4px]'>Compassionate care for a healthier tomorrow</p>
+                <p className='text-body_text_white md:text-center text-justify font-[500] md:text-[20px] text-[15px] md:w-auto w-[450px] md:leading-[26px] leading-[20px] px-[60px]'>Delivering world-class medical care through compassion, innovation, and an unwavering commitment to improving lives. Explore all our healthcare services and experience the Velammal difference</p>
             </div>
         </div>
 
