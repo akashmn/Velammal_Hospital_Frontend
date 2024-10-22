18f46b89d569e4e03495efdcc71dd7adfecb8751
Akash M Nandan <akash003nandan@gmail.com>
2024-10-22 14:49:37 +0530

    use state added for on clicking chab=nge in bg-colour on faqs

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+
+import { useState } from "react";
 import {
   Accordion,
   AccordionContent,
@@ -7,19 +11,25 @@ import {
 
 import Image from "next/image";
 import underline from "@/public/faq_underline.svg";
+import faq_accordian_underline from "@/public/faq_accrodian_underline.svg";
 
 import faqs from "@/json/Faq_data";
 
-
 export function AccordionDemo() {
+  const [openItem, setOpenItem] = useState<string | null>(null);
+
   const headingClasses =
     "text-dark_text_blue font-bold text-[28px] md:text-[42px] leading-[34px] md:leading-[52.5px] capitalize";
   const descriptionClasses =
     "text-body_text_white text-[16px] font-semibold md:text-[20px] leading-[24px] md:leading-[31px] tracking-[0.3px]";
   const triggerClasses =
-    "md:text-center font-medium text-[16px] text-start md:text-[18px] leading-[24px] md:leading-[27.9px] tracking-[0.27px]";
+    "md:text-center font-medium text-[16px] text-start md:text-[18px] leading-[24px] md:leading-[27.9px] tracking-[0.27px] md:px-4 rounded-t-[20px] cursor-pointer";
   const contentClasses =
-    "bg-primary_blue text-white font-normal text-[16px] md:text-[18px] leading-[24px] md:leading-[27.9px] tracking-[0.27px] px-4 md:px-[37px] py-[14px] md:py-[18px] rounded-xl";
+    "bg-primary_blue text-white font-normal text-[16px] md:text-[18px] leading-[24px] md:leading-[27.9px] tracking-[0.27px] px-4 md:px-[37px] py-[14px] md:py-[18px] rounded-b-[20px]";
+
+  const handleToggle = (id: string) => {
+    setOpenItem(openItem === id ? null : id);
+  };
 
   return (
     <div className="md:mt-0 mt-36 flex flex-col items-center justify-center py-[40px] md:py-[70px] gap-[40px] md:gap-[54px]">
@@ -41,13 +51,29 @@ export function AccordionDemo() {
         className="w-full md:w-[630px] flex flex-col items-center justify-center gap-[20px] md:gap-[28px]"
       >
         {faqs.map((faq) => (
-          <AccordionItem key={faq.id} value={faq.id} className="w-full h-auto px-4 md:px-[36px]">
-            <AccordionTrigger className={triggerClasses}>
+          <AccordionItem
+            key={faq.id}
+            value={faq.id}
+            className="w-full h-auto px-4 md:px-[36px]"
+          >
+            <AccordionTrigger
+              className={`${triggerClasses} ${
+                openItem === faq.id ? "bg-blue text-white bg-primary_blue" : ""
+              }`}
+              onClick={() => handleToggle(faq.id)}
+            >
               {faq.question}
             </AccordionTrigger>
-            <AccordionContent className={contentClasses}>
-              {faq.answer}
-            </AccordionContent>
+            <Image
+              src={faq_accordian_underline}
+              alt="faq_accordian_underline"
+              className="h-[1px] w-[10px]"
+            />
+            {openItem === faq.id && (
+              <AccordionContent className={contentClasses}>
+                {faq.answer}
+              </AccordionContent>
+            )}
           </AccordionItem>
         ))}
       </Accordion>
