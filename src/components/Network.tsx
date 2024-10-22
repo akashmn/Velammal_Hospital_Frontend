93e28f5bb656a5ca290050a40ddf9556caab6efe
Akash M Nandan <akash003nandan@gmail.com>
2024-10-22 09:43:20 +0530

    network alignment resolved

--- src/components/Network.tsx
+++ src/components/Network.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import icon from '@/public/arrow_up_right.png';
-import underline from '@/public/hero_underline.png';
+import underline from '@/public/helpdesk_underline.svg'
 import network_image from '@/public/network_velammal_image.svg';
 import networkDetails from '@/json/Network_details'; // Import the actual JSON data
 import { NetworkDetails } from '@/json/Network_details'; // Import the type
@@ -8,12 +8,12 @@ import { NetworkDetails } from '@/json/Network_details'; // Import the type
 const Network = () => {
 
   return (
-    <div className='bg-secondary_white h-auto w-full md:px-[80px] px-[20px] md:py-[82px] py-[40px] flex flex-col items-center justify-center gap-[50px] md:gap-[87px]'>
+    <div className='bg-secondary_white h-auto w-full md:px-[80px] px-[20px] md:py-[82px] py-[40px] flex flex-col items-center justify-center gap-[50px] md:gap-[57px]'>
       <div className='flex flex-col items-center justify-center md:gap-[80px] gap-[25px]'>
         {/* heading & underline */}
         <div className='flex flex-col items-center justify-center md:w-[1290px] w-full md:gap-5 gap-2 px-4'>
           <p className='text-[#011632] text-center font-bold md:font-[600] md:text-[56px] text-[28px] md:leading-[21px] leading-none'>Velammal Healthcare Network</p>
-          <Image src={underline} alt='underline' className='h-[5px] w-[200px]' />
+          <Image src={underline} alt='underline' className='h-[10px] w-[1200px]' />
         </div>
 
         {/* subheading & body */}
@@ -26,9 +26,9 @@ const Network = () => {
       </div>
 
       {/* links and body */}
-      <div className='flex md:flex-row flex-col-reverse items-center justify-center gap-[50px] w-full'>
+      <div className='flex md:flex-row flex-col-reverse items-center justify-center gap-[40px] w-full'>
         {/* redirecting links */}
-        <div className='flex flex-col gap-[20px] md:justify-evenly items-center w-[100%]'>
+        <div className='flex flex-col gap-[27px] md:justify-evenly items-center w-[100%]'>
           {networkDetails.map((link: NetworkDetails, idx:number) => (
             <a key={idx} href={link.url}>
               <button className="md:w-[650px] md:h-[60px] md:py-[12px] py-[10px] md:px-5 px-2 border-2 border-primary_blue flex flex-row items-center justify-between rounded-full gap-[15px] hover:scale-105 transition-all duration-300 ease-in-out hover:drop-shadow-2xl group hover:bg-primary_blue">
@@ -42,12 +42,12 @@ const Network = () => {
         </div>
 
         {/* image and body */}
-        <div className='flex flex-col md:w-[100%] w-full items-center justify-start md:gap-5 gap-[25px]'>
-          <Image src={network_image} alt='network_image' className='md:w-[710px] w-[340px] h-auto' />
-          <p className='md:w-[600px] text-dark_text_blue font-[600] md:text-[24px] text-[17px] text-center md:text-justify md:p-0 px-4 md:leading-[32px] leading-[20px]'>
+        <div className='flex flex-col md:w-[550px] w-full items-center justify-start md:gap-0 md:mr-[50px] gap-[25px]'>
+          <Image src={network_image} alt='network_image' className='md:w-[710px] w-[340px] h-auto'/>
+          <p className='md:w-[550px] text-dark_text_blue font-[600] md:text-[20px] text-[17px] text-center md:text-justify md:p-0 px-4 md:leading-[32px] leading-[20px]'>
             Velammal Medical College hospital and Research Institute, Madurai
           </p>
-          <p className='md:w-[600px] text-body_text_white font-[400] md:text-[19px] text-[14px] md:leading-[26px] leading-[20px] md:px-0 px-[20px] text-justify'>
+          <p className='md:w-[550px] text-body_text_white font-[400] md:text-[19px] text-[14px] md:leading-[26px] leading-[20px] md:px-0 px-[20px] text-justify'>
             Delivering world-class medical care through compassion, innovation, and an unwavering commitment to improving lives. Explore all our healthcare services and experience the Velammal difference.
           </p>
         </div>
