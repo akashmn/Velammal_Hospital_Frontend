8703b5763ee11c3ce8246c1481c3f52930dca039
Manu-Madhu <manum66466@gmail.com>
2024-10-30 00:11:13 +0530

    Update the full part

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,27 +6,29 @@ import line from '@/public/navbar_line.svg';
 
 const Header: React.FC = () => {
     return (
-        <div className="max-sm:hidden h-[34px] w-full bg-accent_blue flex flex-row items-center justify-between px-10 text-white">
-            <div className='flex flex-row items-center gap-10'>
-                <div className='flex flex-row gap-5 items-center justify-center'>
-                    {header_data.map((link, index) => (
-                        <Link key={index} href={link.href}>
-                            <Image src={link.src} alt={link.alt} height={link.height} width={link.width} />
-                        </Link>
-                    ))}
+        <div className="max-sm:hidden h-[34px] w-full bg-accent_blue  text-white">
+            <section className='w-full h-full lg:max-w-[1450px] mx-auto flex flex-row items-center justify-between px-10'>
+                <div className='flex flex-row items-center gap-10'>
+                    <div className='flex flex-row gap-5 items-center justify-center'>
+                        {header_data.map((link, index) => (
+                            <Link key={index} href={link.href}>
+                                <Image src={link.src} alt={link.alt} height={link.height} width={link.width} />
+                            </Link>
+                        ))}
+                    </div>
+                    <div className='flex flex-row gap-8 items-center'>
+                        <p className='text-[13px] font-normal leading-[12px] tracking-[0.4px] text-white'>Book your Appointment</p>
+                        <p className='text-[16px] font-normal tracking-[0.64px] text-[#F7F9FE]"'>0452 711 33 33</p>
+                    </div>
                 </div>
-                <div className='flex flex-row gap-8 items-center'>
-                    <p className='text-[13px] font-normal leading-[12px] tracking-[0.4px] text-white'>Book your Appointment</p>
-                    <p className='text-[16px] font-normal tracking-[0.64px] text-[#F7F9FE]"'>0452 711 33 33</p>
+                <div className="flex flex-row items-center justify-center gap-2 text-[12px] font-normal leading-[12px] tracking-[0.4px] font-figtree text-white">
+                    <Link href="/">Privacy Policy</Link>
+                    <Image src={line} alt=''></Image>
+                    <Link href="/">Disclaimer</Link>
+                    <Image src={line} alt=''></Image>
+                    <Link href="/">Contact</Link>
                 </div>
-            </div>
-            <div className="flex flex-row items-center justify-center gap-2 text-[12px] font-normal leading-[12px] tracking-[0.4px] font-figtree text-white">
-                <Link href="/">Privacy Policy</Link>
-                <Image src={line} alt=''></Image>
-                <Link href="/">Disclaimer</Link>
-                <Image src={line} alt=''></Image>
-                <Link href="/">Contact</Link>
-            </div>
+            </section>
         </div>
     );
 };
